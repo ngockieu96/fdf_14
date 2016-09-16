09ecbce1d443716f91a35a93c0857d4973ef47b0
Tran Dinh Vi <trandinhvi39@gmail.com>
2016-09-16 10:52:39 +0700

    Create Migration

---
+++ database/migrations/2016_08_23_013110_modify_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable();
+            $table->string('phone');
+            $table->string('address');
+            $table->tinyInteger('role')->default(0);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn(['avatar', 'phone', 'address', 'role']);
+        });
+    }
+}
---
+++ database/migrations/2016_09_15_041525_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->double('price');
+            $table->boolean('status');
+            $table->string('image');
+            $table->integer('quantity');
+            $table->double('rate_average')->nullable();
+            $table->integer('rate_count')->nullable();
+            $table->integer('view_count')->nullable();
+            $table->integer('category_id')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
